211be7344dd25460bf709d2004345914e4405f93
MilenaKovalski <milenakovalski@gmail.com>
2024-12-01 16:51:06 -0300

    Carrinho nas frutas

--- lib/src/auth/Pages/home/Components/item_tile.dart
+++ lib/src/auth/Pages/home/Components/item_tile.dart
@@ -1,33 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/src/auth/Models/item_model.dart';
+import 'package:flutter_application_1/src/auth/services/utils_services.dart';
 
 class ItemTile extends StatelessWidget {
   final ItemModel item;
 
-  const ItemTile({
+  ItemTile({
     Key? key,
     required this.item,
   }) : super(key: key);
 
+  final UtilsServices utilsServices = UtilsServices();
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      shadowColor: Colors.grey.shade300,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Column(
-        children: [
-          //imagem
-          Image.asset(item.imgUrl),
+    return Stack(
+      children: [
+        Card(
+          elevation: 2,
+          shadowColor: Colors.grey.shade300,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                //imagem
+                Expanded(child: Image.asset(item.imgUrl)),
 
-          //Nome
-          Text(item.itemName),
+                //Nome
+                Text(
+                  item.itemName,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
 
-          //preço - unidade
-        ],
-      ),
+                //preço - unidade
+                Row(
+                  children: [
+                    Text(
+                      utilsServices.priceToCurrency(item.price),
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 137, 202, 117)),
+                    ),
+                    Text(
+                      '/${item.unit}',
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          top: 4,
+          right: 4,
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: 40,
+              width: 35,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: const Icon(
+                Icons.add_shopping_cart_outlined,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
